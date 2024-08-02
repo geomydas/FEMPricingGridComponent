cce9ee5a83584458e6ac4bf0ed74df74c614ef22
geomydas <155147963+geomydas@users.noreply.github.com>
2024-08-02 14:10:59 +0800

    first commit, added files

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: { karla: ["Karla", "sans-serif"] },
+    colors: {
+      cyan: "hsl(179, 62%, 43%)",
+      brightyellow: "hsl(71, 73%, 54%)",
+      lightgray: "hsl(204, 43%, 93%)",
+      grayishblue: "hsl(218, 22%, 67%)",
+      white: "#ffffff",
+      lightcyan: "#4abebd",
+    },
+  },
+  plugins: [],
+};
